fd69db52fd6591666215304d7770e138c1290143
jonah <jonahkf@gmail.com>
2024-12-09 21:48:30 +0900

    FINISHED: Recursion Project
    Earlier error was due to how Zed shows long arrays
    in console. No issues with mergeSort().

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -33,9 +33,4 @@ const merge = (leftArr, rightArr) => {
 
 const testArr = [3, 2, 1, 13, 8, 5, 0, 1];
 
-// Demonstration of splitting arr in half
-// console.log(testArr.slice(0, Math.floor(testArr.length / 2)));
-// console.log(testArr.slice(Math.floor(testArr.length / 2), testArr.length));
-
 console.log(mergeSort(testArr));
-// Expecting "[0, 1, 1, 2, 3, 5, 8, 13]"
