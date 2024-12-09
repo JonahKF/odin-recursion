2bc1799c634b90723632b08a1013b409122db855
jonah <jonahkf@gmail.com>
2024-12-09 21:39:05 +0900

    mergeSort() and merge() work, but the output is weird.
    Result is:
    [
      0, 1, 1,  2,
      3, 5, 8, 13
    ]
    Spaced weirdly.

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -9,11 +9,26 @@ const mergeSort = (arr) => {
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
 
-  const merge = (leftArr, rightArr) => {
-    let newArr = [];
-  };
-
   return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (leftArr, rightArr) => {
+  let newArr = [];
+  while (leftArr.length !== 0 && rightArr.length !== 0) {
+    if (leftArr[0] <= rightArr[0]) {
+      newArr.push(leftArr.shift());
+    } else {
+      newArr.push(rightArr.shift());
+    }
+  }
+  while (leftArr.length !== 0) {
+    newArr.push(leftArr.shift());
+  }
+  while (rightArr.length !== 0) {
+    newArr.push(rightArr.shift());
+  }
+
+  return newArr;
 };
 
 const testArr = [3, 2, 1, 13, 8, 5, 0, 1];
